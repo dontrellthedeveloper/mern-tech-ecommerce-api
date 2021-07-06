b2a419f032a43f04efcc95082f18b6e2feec73bf
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-07-06 09:10:21 -0400

    setup firebase admin

---
+++ firebase/index.js
@@ -0,0 +1,7 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../config/fbServiceAccountKey");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
