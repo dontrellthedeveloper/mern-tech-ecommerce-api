f3ff2f8e81b9f31aa799405a655e94a67aa37280
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-07-06 12:14:36 -0400

    setup basic middleware

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
-const {createOrUpdateUser} = require('../controllers/auth');
+// middlewares
+const { authCheck } = require("../middlewares/auth");
 
-router.get("/create-or-update-user", createOrUpdateUser);
+// controller
+const { createOrUpdateUser } = require("../controllers/auth");
+
+
+router.post("/create-or-update-user", authCheck, createOrUpdateUser);
+
 
 module.exports = router;
