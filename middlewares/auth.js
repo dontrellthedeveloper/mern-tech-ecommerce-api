73cae75a907262339d0880c782ec3a87447c7796
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-07-07 17:46:39 -0400

    added admin check middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,7 @@
 
 require("../firebase");
 const admin = require("firebase-admin");
+const User = require("../models/user");
 
 exports.authCheck = async (req, res, next) => {
     console.log(req.headers); // token
@@ -19,5 +20,21 @@ exports.authCheck = async (req, res, next) => {
         res.status(401).json({
             err: "Invalid or expired token",
         });
+    }
+};
+
+
+
+exports.adminCheck = async (req, res, next) => {
+    const { email } = req.user;
+
+    const adminUser = await User.findOne({ email }).exec();
+
+    if (adminUser.role !== "admin") {
+        res.status(403).json({
+            err: "Admin resource. Access denied.",
+        });
+    } else {
+        next();
     }
 };
