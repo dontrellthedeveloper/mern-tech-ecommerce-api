78d2530e0e2a7fc5984880914fdea1e06499cca5
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-07-07 18:55:41 -0400

    added category routes

---
+++ controllers/category.js
@@ -0,0 +1,21 @@
+const Category = require("../models/category");
+
+exports.create = async (req, res) => {
+
+};
+
+exports.list = async (req, res) => {
+
+};
+
+exports.read = async (req, res) => {
+
+};
+
+exports.update = async (req, res) => {
+
+};
+
+exports.remove = async (req, res) => {
+
+};
