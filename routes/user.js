744ace531bb1e3f088d978e433ec110f796f4746
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-07-11 23:30:47 -0400

    added cash order backend

--- routes/user.js
+++ routes/user.js
@@ -9,13 +9,15 @@ const { userCart, getUserCart, emptyCart, saveAddress, applyCouponToUserCart, cr
     addToWishlist,
     wishlist,
     removeFromWishlist,
+    createCashOrder,
 } = require("../controllers/user");
 
 router.post("/user/cart", authCheck, userCart); // save cart
 router.get("/user/cart", authCheck, getUserCart); // get cart
 router.delete("/user/cart", authCheck, emptyCart); // empty cart
 router.post("/user/address", authCheck, saveAddress);
-router.post("/user/order", authCheck, createOrder);
+router.post("/user/order", authCheck, createOrder); // stripe
+router.post("/user/cash-order", authCheck, createCashOrder); // cod
 router.get("/user/orders", authCheck, orders);
 // coupon
 router.post("/user/cart/coupon", authCheck, applyCouponToUserCart);
